a7323a7706a2c59522a48026ff1bb6d2f68513cd
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2021-12-30 13:29:09 -0500

    Added Discord support

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const { Authenticator } = require('minecraft-launcher-core');
 var childProcess = require("child_process");
 var path = require("path");
 const fs = require("fs");
+const rpc = require("discord-rpc");
+const client = new rpc.Client({ transport: 'ipc' });
 
+client.login({ clientId : '926165825843494933' }).catch(console.error); 
   
   function createWindow () {
     const win = new BrowserWindow({
@@ -70,3 +73,37 @@ function signOut() {
   fs.unlinkSync(process.env.APPDATA + "/warden/auth/auth.json");
   fs.unlinkSync(process.env.APPDATA + "/warden/auth/auth_profile.json");
 }
+
+// client.updatePresence({
+//   state: 'Poking Around',
+//   startTimestamp: Date.now(),
+//   largeImageKey: 'warden_head',
+//   smallImageKey: 'warden_head',
+//   instance: true
+// });
+
+client.on('ready', () => {
+  console.log('Your presence works now check your discord profile :D')
+  client.request('SET_ACTIVITY', {
+  pid: process.pid,
+  activity: {
+      state: 'Poking Around',
+  assets: {
+           large_image: 'warden_head',
+  },
+  timestamps: {
+    start: Date.now(),
+  },
+  // buttons : [
+  //     {
+  //         label : config.Button1,url : config.Url1
+  //     },
+  //     { 
+  //         label : config.Button2,url : config.Url2
+  //     },
+  //   //labels are the buttons that you wanna provide to your rich presence and urls are the links that leads you when someone press that button
+  //   //Note the button won't work for you but don't worry it work for others
+  //     ]
+      }
+  })
+  })
